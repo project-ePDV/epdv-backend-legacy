d6b6afae1752a9133df5952e5a82589316e7d051
Kal Nascimento <kalebe.nascimento19@gmail.com>
2023-03-12 15:17:25 -0300

    feat: create address, costumer, request, productRequest migrations

---
+++ app/Database/Migrations/2023-03-12-173042_CreateTableAddress.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTableAddress extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'address' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'null'       => true
+            ],
+            'number' => [
+                'type' => 'VARCHAR',
+                'constraint' => '5',
+                'null' => false
+            ],
+            'district' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'null'       => true
+            ],
+            'city' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50',
+                'null' => false
+            ],
+            'state' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '50',
+                'null'       => true
+            ],
+            'cep' => [
+                'type' => 'VARCHAR',
+                'constraint' => '8',
+                'null' => false
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('address');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('address');
+    }
+}
---
+++ app/Database/Migrations/2023-03-12-173700_CreateTableRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTableRequest extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'date' => [
+                'type'       => 'DATE',
+                'null'       => false
+            ],
+            'value' => [
+                'type' => 'DECIMAL',
+                'constraint' => '10,2',
+                'null' => false
+            ],
+            'fk_costumer' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '11',
+                'null'       => false
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('fk_costumer', 'costumer', 'cpf', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('request');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('request');
+    }
+}
