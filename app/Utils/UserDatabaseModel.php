0accd6ed8f834b44e47af86c67e0c37a29605ad9
Kal Nascimento <kalebe.nascimento19@gmail.com>
2023-03-25 11:25:53 -0300

    Refactor: move from models to utils

--- app/Utils/UserDatabaseModel.php
+++ app/Utils/UserDatabaseModel.php
@@ -1,15 +1,17 @@
 <?php
 
-namespace App\Models;
+namespace App\Utils;
 
 class UserDatabaseModel
 {
   private $name;
   private $password;
+  private $host;
 
-  public function __construct($name, $password = '') {
+  public function __construct($name, $password = 'admin123', $host = 'db_epdv') {
     $this->name = $name;
     $this->password = $password;
+    $this->host = $host;
   }
 
   public function createUserDB()
@@ -19,25 +21,29 @@ class UserDatabaseModel
     $forge->createDatabase($this->name, true);
 
     $db = [
-      'DSN'      => '',
-      'hostname' => 'localhost',
+      'hostname' => $this->host,
       'username' => 'root',
       'password' => $this->password,
       'database' => $this->name,
       'DBDriver' => 'MySQLi',
-      'DBPrefix' => '',
       'pConnect' => false,
       'DBDebug'  => (ENVIRONMENT !== 'production'),
-      'charset'  => 'utf8',
-      'DBCollat' => 'utf8_general_ci',
-      'swapPre'  => '',
-      'encrypt'  => false,
-      'compress' => false,
-      'strictOn' => false,
-      'failover' => [],
       'port'     => 3306,
     ];
 
     return \Config\Database::connect($db, false);
+  }
+
+  public function getConnection() {
+    return [
+      'hostname' => 'db_epdv',
+      'username' => 'root',
+      'password' => 'admin123',
+      'database' => $this->name,
+      'DBDriver' => 'MySQLi',
+      'pConnect' => false,
+      'DBDebug'  => (ENVIRONMENT !== 'production'),
+      'port'     => 3306,
+    ];
   }
 }
