893939077b018cbc57d01f486648cac89c1a39c6
Kal Nascimento <kalebe.nascimento19@gmail.com>
2023-06-01 17:58:16 -0300

    deploy: add base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://dev-epdv.onrender.com/';
+    public $baseURL = 'http://localhost:8080';
 
     /**
      * --------------------------------------------------------------------------
