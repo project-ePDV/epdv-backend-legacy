d8e80f17d126fd1be10e7676a9a53cbb8256d31f
Kalebe Nascimento <60221532+kalNascimento@users.noreply.github.com>
2023-06-22 23:29:59 -0300

    Update run.sh,add echo

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 echo 'Initializing....'
+echo 'Instaling dependeces....'
 composer install
+
+echo 'Starter mysql....'
 service mysql start
+
+echo 'Login mysql....'
 mysql -u root -h localhost -e 'create database db_epdv'
+
+echo 'Starter apache server....'
 apache2-foreground -D Listen=0.0.0.0:3000
