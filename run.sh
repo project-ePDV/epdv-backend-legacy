7c2ff326417e3609449bbcae4491b9281e8e9c7e
Kalebe Nascimento <60221532+kalNascimento@users.noreply.github.com>
2023-06-22 23:14:15 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'hello world'
-composer update
-sudo service mysql start
-mysql -u root -h localhost -p admin123 -e 'create database db_epdv'
+composer install
+service mysql start
+mysql -u root -h localhost -e 'create database db_epdv'
 apache2-foreground -D Listen=0.0.0.0:3000
